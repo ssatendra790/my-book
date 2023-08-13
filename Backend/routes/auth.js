63297b00808672aee3d0d8412011e01e1f302cb7
ssatendra790 <ssatendra790@gmail.com>
2023-08-13 14:10:54 +0530

    nds

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
+const {body, validationResult} = require('express-validator');
 
 router.use(express.json());
 
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body)
+router.post('/',[
+        body('name', 'Enter a valid name').isLength({min: 4}),
+        body('email', 'Enter a valid email').isEmail(),
+        body('password', 'Password is too weak').isLength({min: 5}),
+    ], (req, res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        }).then(user => res.json(user));
 })
 
 module.exports = router
