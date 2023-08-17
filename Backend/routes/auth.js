dcf7ee1e3a75b6a025695e51616ae847db26d29b
ssatendra790 <ssatendra790@gmail.com>
2023-08-17 12:58:38 +0530

    17 Aug

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,13 +4,13 @@ const router = express.Router()
 const {body, validationResult} = require('express-validator');
 
 router.use(express.json());
-
 router.post('/',[
         body('name', 'Enter a valid name').isLength({min: 4}),
         body('email', 'Enter a valid email').isEmail(),
         body('password', 'Password is too weak').isLength({min: 5}),
     ], (req, res)=>{
         const errors = validationResult(req);
+
         if(!errors.isEmpty()){
             return res.status(400).json({errors: errors.array()});
         }
@@ -18,7 +18,11 @@ router.post('/',[
             name: req.body.name,
             email: req.body.email,
             password: req.body.password,
-        }).then(user => res.json(user));
+        }).then(user => res.json(user))
+        .catch(err=>{
+            console.log("Error is ", err)
+            res.json({error: 'Email already exists.', message: err.message})
+        })
 })
 
 module.exports = router
